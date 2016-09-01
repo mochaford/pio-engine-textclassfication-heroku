fbf118128aee740f9776f15f08b5dc85fbafebf1
Rajdeep Dua <dua_rajdeep@yahoo.com>
2016-09-01 11:16:44 +0530

    updated engine id

--- src/main/scala/Engine.scala
+++ src/main/scala/Engine.scala
@@ -3,12 +3,14 @@ package org.template.textclassification
 import io.prediction.controller.IEngineFactory
 import io.prediction.controller.Engine
 
-/** Define Query class which serves as a wrapper for
+/**
+  * Define Query class which serves as a wrapper for
   * new text data.
   */
 case class Query(text: String) extends Serializable
 
-/** Define PredictedResult class which serves as a
+/**
+  * Define PredictedResult class which serves as a
   * wrapper for a predicted class label and the associated
   * prediction confidence.
   */
@@ -16,12 +18,16 @@ case class PredictedResult(
   category: String,
   confidence: Double) extends Serializable
 
-/** Define ActualResult class which serves as a wrapper
+/**
+  * Define ActualResult class which serves as a wrapper
   * for an observation's true class label.
   */
 case class ActualResult(category: String) extends Serializable
 
-/** Define Engine */
+/**
+  * Define Engine
+  *
+  * */
 object TextClassificationEngine extends IEngineFactory {
   def apply() = {
     new Engine(
